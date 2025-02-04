8ced14c4848a18aaa502dbcc30d1e82fea2b5fd5
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-02-04 21:59:32 +0530

    Add files via upload

---
+++ Html_elements_with_js/index.js
@@ -0,0 +1,9 @@
+// document.getElementsByTagName("li");  //shows all list elements
+// document.getElementsByTagName("li")[2].style.color="purple";
+// document.getElementsByTagName("li").length;
+// document.getElementsByClassName("btn");
+document.getElementsById("title");
+document.getElementsById("title").innerHTML="GOODBYE!!";
+document.querySelector("li a");
+document.querySelector("#list a");
+document.querySelectorAll("#list .item");
