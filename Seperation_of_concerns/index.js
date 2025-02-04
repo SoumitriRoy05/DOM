dd3c63ee8df7a63a1a8909e987f2c6039af65f90
SOUMITRI ROY <soumitriroy05@gmail.com>
2025-02-04 21:58:25 +0530

    Add files via upload

---
+++ Seperation_of_concerns/index.js
@@ -0,0 +1,5 @@
+document.querySelector("button").classList;
+document.querySelector("button").classList.add("invisible");
+document.querySelector("button").classList.toggle("invisible");
+
+document.querySelector("h1").classList.add("huge");
